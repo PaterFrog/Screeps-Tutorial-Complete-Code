188f74993295aee0f2d16c256e50068e2fe74447
PaterFrog <46433636+PaterFrog@users.noreply.github.com>
2022-02-02 23:05:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ module.exports.loop = function () {
     if(harvesters.length < 2) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY ,CARRY,MOVE, MOVE], newName, 
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, 
             {memory: {role: 'harvester'}});
     }
     
